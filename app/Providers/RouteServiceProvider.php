f24ca860f91beed3e0b04f0b18516169d4af881b
Juan López <juanlopez.developer@gmail.com>
2018-07-10 22:50:07 -0500

    Modelos y migraciones

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,8 +65,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
+        //Route::prefix('api') // Remove prefix
+        Route::middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
